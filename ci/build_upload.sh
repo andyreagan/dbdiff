959c9a5bed77529645e07731223ee7bf16216884
Andy Reagan <andy@andyreagan.com>
2022-05-05 13:57:13 -0400

    Create python-publish.yml (#2)
    * Create python-publish.yml
    
    * Also update docker container.

--- ci/build_upload.sh
+++ ci/build_upload.sh
@@ -1,6 +1,6 @@
-pyenv global 3.7.12 3.8.12 3.9.9 3.10.0
+pyenv global 3.7.13 3.8.13 3.9.12 3.10.4
 python3 setup.py sdist -d dist
 python3 -m twine check -- dist/*.tar.gz
 python3 -m tox --installpkg dist/*.tar.gz
 python3 -m twine upload dist/*
-pyenv global 3.10.0
+pyenv global 3.10.4
